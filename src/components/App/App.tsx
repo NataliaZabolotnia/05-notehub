bc2c44c0b41e499f36967fc94c1c28a0755f3449
Natalia Zabolotnia <zanatav78@gmail.com>
2025-08-15 19:15:02 +0300

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import css from "./App.module.css"
+// import axios from "axios"
+import "modern-normalize/modern-normalize.css"
+
+export default function App () {
+
+
+  return(
+  <div className={css.app}>
+	<header className={css.toolbar}>
+		{/* Компонент SearchBox */}
+		{/* Пагінація */}
+		{/* Кнопка створення нотатки */}
+  </header>
+</div>  
+  )
+}
